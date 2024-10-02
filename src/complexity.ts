cae1617a0218c786b1559f3a990f77daf4842b6f
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2024-10-02 23:06:58 +0200

    Add option to override default config.

--- src/complexity.ts
+++ src/complexity.ts
@@ -1,35 +1,50 @@
 import typescriptParser from '@typescript-eslint/parser';
-import { ConfigOrTsPath } from './types';
+import { deepMerge } from '@typescript-eslint/utils/eslint-utils';
+import { Config, ConfigOrTsPath } from './types';
 
-const config = (config: ConfigOrTsPath) => ([
-    {
-        name: '@patrikvalkovic/eslint-config/complexity',
-        ...(!config ? {} : typeof config === 'string' ? {
+const config = (config: ConfigOrTsPath, overrides?: Config) => {
+    const configToUse = deepMerge(
+        typeof config === 'string' ? {
+            settings: {
+                'eslint-plugin-import/resolver': {
+                    typescript: {
+                        project: [config],
+                    },
+                },
+            },
             languageOptions: {
                 parser: typescriptParser,
                 parserOptions: {
                     project: config,
                 },
             },
-        } : config),
-        rules: {
-            //  ╔═══════════════════════╗
-            //  ║                       ║
-            //  ║      Suggestions      ║
-            //  ║                       ║
-            //  ╚═══════════════════════╝
-            'complexity': 'warn',
-            'id-denylist': 'warn',
-            'id-length': 'warn',
-            'max-classes-per-file': 'warn',
-            'max-depth': 'warn',
-            'max-lines': 'warn',
-            'max-lines-per-function': 'warn',
-            'max-nested-callbacks': 'warn',
-            'max-params': 'warn',
-            'max-statements': 'warn',
+        } : config,
+        overrides,
+    );
+
+    return ([
+        {
+            ...configToUse,
+            name: '@patrikvalkovic/eslint-config/complexity',
+            rules: {
+                //  ╔═══════════════════════╗
+                //  ║                       ║
+                //  ║      Suggestions      ║
+                //  ║                       ║
+                //  ╚═══════════════════════╝
+                'complexity': 'warn',
+                'id-denylist': 'warn',
+                'id-length': 'warn',
+                'max-classes-per-file': 'warn',
+                'max-depth': 'warn',
+                'max-lines': 'warn',
+                'max-lines-per-function': 'warn',
+                'max-nested-callbacks': 'warn',
+                'max-params': 'warn',
+                'max-statements': 'warn',
+            },
         },
-    },
-]);
+    ]);
+};
 
 export = config;
